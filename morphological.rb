ddbc6f60102360cf9612b9966da0776e821ebc35
hiroponsan <Chuo-univTanaka@outlook.jp>
2016-01-05 15:46:34 +0900

    commit

---
+++ morphological.rb
@@ -0,0 +1,15 @@
+require 'open-uri'
+require "nokogiri"
+
+APPID = 'dj0zaiZpPVVFMTc4ZEdBOGRHeCZzPWNvbnN1bWVyc2VjcmV0Jng9OTU-'
+REQUEST_URL = "http://jlp.yahooapis.jp/MAService/V1/parse"
+
+module Morphological
+  extend self
+  def split(sentence, appid=APPID, results="ma", filter="1|2|3|4|5|9|10")
+    params = "?appid=#{APPID}&results=#{results}&filter=#{URI.encode(filter)}&sentence=#{URI.encode(sentence)}"
+    doc = Nokogiri::HTML(open(REQUEST_URL + params))
+    surfaces = doc.xpath('//word/surface').map{|i| i.text} rescue nil
+    surfaces
+  end
+end
